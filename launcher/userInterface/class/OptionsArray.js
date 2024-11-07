c81daccaa7f00af6dd1477edd8ca639ed2211cc0
Josh <joshuat04@googlemail.com>
2024-11-07 20:13:09 +0000

    Improvements and fixes (additional) - Version: dev_Interface_alpha_0.7

--- launcher/userInterface/class/OptionsArray.js
+++ launcher/userInterface/class/OptionsArray.js
@@ -3,7 +3,7 @@
 import MenuOption from "anymuz-interaction/MenuOption";
 import TypeValidator from "#internal/TypeValidation";
 //---------------- //
-// CLASS OptionsArray - Specialised Array for holding purely menu objects:
+// CLASS OptionsArray - Specialised Array for holding purely menu option:
 // ----------------------------------------------------------------------- //
 export default class OptionsArray extends Array{constructor(...args){super(...args)
     return new Proxy(this,{set:(target,property,value)=>{if(property!=='length'){value=TypeValidator.typeCheck(value,MenuOption)}
